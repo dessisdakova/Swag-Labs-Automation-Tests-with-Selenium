340a44417f2590919fbaa4704485f3c0d8430388
Desislava Dakova <dessisdakova@Desislavas-MacBook-Pro.local>
2025-03-12 18:02:32 +0200

    Added messages for tests

--- tests/test_burger_menu.py
+++ tests/test_burger_menu.py
@@ -8,7 +8,8 @@ def test_can_be_opened_in_inventory_page(logged_in_driver):
 
     inventory_page.open_menu()
 
-    assert inventory_page.get_nav_element_visibility()
+    assert inventory_page.get_nav_element_visibility(), \
+        "Burger menu is not opened in inventory page.."
 
 
 def test_can_be_opened_in_cart_page(logged_in_driver):
@@ -17,4 +18,5 @@ def test_can_be_opened_in_cart_page(logged_in_driver):
 
     cart_page.open_menu()
 
-    assert cart_page.get_nav_element_visibility()
+    assert cart_page.get_nav_element_visibility(), \
+        "Burger menu is not opened in cart page."
--- tests/test_inventory_page.py
+++ tests/test_inventory_page.py
@@ -8,7 +8,8 @@ def test_products_sort_by_name_a_to_z(logged_in_driver):
     inventory_page.select_sorting("Name (A to Z)")
     product_names = inventory_page.get_product_names()
 
-    assert product_names == sorted(product_names), "Products are not sorted alphabetically (A to Z)!"
+    assert product_names == sorted(product_names), \
+        "Products are not sorted alphabetically (A to Z)!"
 
 
 def test_products_sorted_by_name_z_to_a(logged_in_driver):
@@ -18,7 +19,8 @@ def test_products_sorted_by_name_z_to_a(logged_in_driver):
     inventory_page.select_sorting("Name (Z to A)")
     product_names = inventory_page.get_product_names()
 
-    assert product_names == sorted(product_names, reverse=True), "Products are not sorted Z to A"
+    assert product_names == sorted(product_names, reverse=True), \
+        "Products are not sorted Z to A"
 
 
 def test_products_sorted_by_price_low_to_high(logged_in_driver):
@@ -28,7 +30,8 @@ def test_products_sorted_by_price_low_to_high(logged_in_driver):
     inventory_page.select_sorting("Price (low to high)")
     product_prices = inventory_page.get_product_prices()
 
-    assert product_prices == sorted(product_prices), "Products are not sorted by price low to high"
+    assert product_prices == sorted(product_prices), \
+        "Products are not sorted by price low to high"
 
 
 def test_products_sorted_by_price_high_to_low(logged_in_driver):
@@ -38,7 +41,8 @@ def test_products_sorted_by_price_high_to_low(logged_in_driver):
     inventory_page.select_sorting("Price (high to low)")
     product_prices = inventory_page.get_product_prices()
 
-    assert product_prices == sorted(product_prices, reverse=True), "Products are not sorted by price high to low"
+    assert product_prices == sorted(product_prices, reverse=True), \
+        "Products are not sorted by price high to low"
 
 
 def test_products_can_be_added_to_cart(logged_in_driver):
@@ -50,7 +54,8 @@ def test_products_can_be_added_to_cart(logged_in_driver):
     for button in add_buttons:
         button.click()
 
-    assert inventory_page.get_items_in_cart() == 6
+    assert inventory_page.get_items_in_cart() == 6, \
+        "All products have not been added to cart."
 
 
 def test_products_can_be_removed_from_cart(logged_in_driver):
@@ -64,7 +69,8 @@ def test_products_can_be_removed_from_cart(logged_in_driver):
     for button in remove_buttons:
         button.click()
 
-    assert inventory_page.get_items_in_cart() == 0
+    assert inventory_page.get_items_in_cart() == 0, \
+        "All products have not been removed from cart."
 
 
 def test_shopping_cart_link_redirects_to_cart(logged_in_driver):
@@ -73,5 +79,6 @@ def test_shopping_cart_link_redirects_to_cart(logged_in_driver):
 
     inventory_page.go_to_cart()
 
-    assert inventory_page.get_current_url() == "https://www.saucedemo.com/cart.html"
+    assert inventory_page.get_current_url() == "https://www.saucedemo.com/cart.html", \
+        "User is not redirected to correct page."
 
