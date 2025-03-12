397f2223d854d517cc5736209fb0e82eac912a43
Desislava Dakova <dessisdakova@Desislavas-MacBook-Pro.local>
2025-03-12 18:04:08 +0200

    Updated cart page tests

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
 from pages.base_page import BasePage
 from pages.burger_menu import BurgerMenu
@@ -7,6 +8,7 @@ from pages.burger_menu import BurgerMenu
 class CartPage(BasePage, BurgerMenu):
 
     items_in_cart = (By.CSS_SELECTOR, 'div[data-test="inventory-item-name"]')
+    remove_buttons = (By.CSS_SELECTOR, "button.btn_small")
     continue_shopping_button = (By.CSS_SELECTOR, 'button[data-test="continue-shopping"]')
     checkout_button = (By.CSS_SELECTOR, 'button[data-test="checkout"]')
     explicit_wait_element = (By.CSS_SELECTOR, "span[data-test='title']")
@@ -23,7 +25,7 @@ class CartPage(BasePage, BurgerMenu):
     def explicit_wait_locator(self) -> tuple[str, str]:
         return CartPage.explicit_wait_element
 
-    def get_items_in_cart(self):
+    def get_items_in_cart(self) -> list[WebElement]:
         return self.driver.find_elements(*CartPage.items_in_cart)
 
     def click_continue_shopping_button(self):
@@ -31,3 +33,6 @@ class CartPage(BasePage, BurgerMenu):
 
     def click_checkout_button(self):
         self.driver.find_element(*CartPage.checkout_button).click()
+
+    def get_remove_buttons(self) ->list[WebElement]:
+        return self.driver.find_elements(*CartPage.remove_buttons)
---
+++ tests/test_cart_page.py
@@ -0,0 +1,36 @@
+from pages.cart_page import CartPage
+from pages.inventory_page import InventoryPage
+
+
+def test_continue_shopping_button_redirects_to_inventory_page(logged_in_driver):
+    cart_page = CartPage(logged_in_driver)
+    cart_page.open()
+
+    cart_page.click_continue_shopping_button()
+
+    assert cart_page.get_current_url() == "https://www.saucedemo.com/inventory.html", \
+        "User is not redirected to correct page."
+
+
+def test_correct_product_is_added_to_cart(logged_in_driver):
+    inventory_page = InventoryPage(logged_in_driver)
+    inventory_page.open()
+    backpack_add_button = inventory_page.get_add_to_cart_buttons()[0]
+    backpack_add_button.click()
+    inventory_page.go_to_cart()
+
+    cart_page = CartPage(logged_in_driver)
+    cart_page.open()
+    first_product_name = cart_page.get_items_in_cart()[0].text
+
+    assert len(cart_page.get_items_in_cart()) == 1, "Item was not added to the cart."
+    assert first_product_name == "Sauce Labs Backpack", "Different item was added to cart."
+
+
+def test_product_can_be_removed_from_cart(logged_in_driver):
+    cart_page = CartPage(logged_in_driver)
+    cart_page.open()
+    remove_btn = cart_page.get_remove_buttons()[0]
+    remove_btn.click()
+
+    assert len(cart_page.get_items_in_cart()) == 0, "Item was not removed from cart."
